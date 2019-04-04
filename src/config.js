c2963f91cbc45466e04c91cc9b0dc8a397c4c7e1
petersen-rex <petersen.rex@gmail.com>
2019-04-03 20:19:47 -0500

    Configure prod and dev environments settings

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
-export default {
-  MAX_ATTACHMENT_SIZE: 5000000,
+const prod = {
   s3: {
     REGION: "us-east-1",
     BUCKET: "notes-app-2-api-prod-attachmentsbucket-v9i3b6b9xt0m"
@@ -15,3 +14,30 @@ export default {
     IDENTITY_POOL_ID: "us-east-1:cc4e4465-883d-464b-87da-3753e31cab35"
   }
 };
+
+const dev = {
+	  s3: {
+	    REGION: "us-east-1",
+	    BUCKET: "notes-app-2-api-dev-attachmentsbucket-1o95usyjx6jvu"
+	  },
+	  apiGateway: {
+	    REGION: "us-east-1",
+	    URL: "https://api.openelastic.org/dev"
+	  },
+	  cognito: {
+	    REGION: "us-east-1",
+	    USER_POOL_ID: "us-east-1_FiFXEDDxh",
+	    APP_CLIENT_ID: "68l6ufmr6evmeegt6a64p004tk",
+	    IDENTITY_POOL_ID: "us-east-1:9603adbb-5f16-4234-9fec-3602d2951a2b"
+	  }
+};
+
+//Default to dev if not set
+const config = process.env.REACT_APP_STAGE === 'prod'
+  ? prod
+  : prod;
+
+export default {
+	  MAX_ATTACHMENT_SIZE: 5000000,
+	  ...config
+}
